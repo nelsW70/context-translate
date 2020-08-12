e5b061429010447b9a2b464ac0cd70aa087532e6
Nelson Foltz <nelsfoltz@gmail.com>
2020-08-12 16:57:27 -0400

    add: implement consumer in Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import LanguageContext from '../context/LanguageContext'
+import LanguageContext from '../contexts/LanguageContext'
 
 class Button extends React.Component {
-  static contextType = LanguageContext
-
+  renderSubmit(value) {
+    return value === 'english' ? 'Submit' : 'Voorleggen'
+  }
   render() {
-    console.log(this.context)
-    return <button className="ui button primary">Submit</button>
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {value => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
+    )
   }
 }
 

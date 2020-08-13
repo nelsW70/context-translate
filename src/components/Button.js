6bf74d6af983368eb9e8588dc0a4cc08fd585e4e
Nelson Foltz <nelsfoltz@gmail.com>
2020-08-13 08:47:22 -0400

    add: LanguageSelector comp

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,16 +6,20 @@ class Button extends React.Component {
   renderSubmit(value) {
     return value === 'english' ? 'Submit' : 'Voorleggen'
   }
+
+  renderButton(color) {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {value => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
+    )
+  }
   render() {
     return (
       <ColorContext.Consumer>
-        {color => (
-          <button className={`ui button ${color}`}>
-            <LanguageContext.Consumer>
-              {value => this.renderSubmit(value)}
-            </LanguageContext.Consumer>
-          </button>
-        )}
+        {color => this.renderButton(color)}
       </ColorContext.Consumer>
     )
   }
